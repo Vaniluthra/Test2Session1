519ff441db7bc7bea74ff0f276c7e577fe3fdc8b
vaniluthra <94587714+Vaniluthra@users.noreply.github.com>
2022-02-24 16:52:08 +0000

    t

--- p1original.c
+++ p1original.c
@@ -1,2 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+//gcc -g p1original.c -lm
+void input(float *x1, float *y1, float *x2, float *y2)
+{
+  printf("Enter: ");
+  scanf("%f%f%f%f",x1,y1,x2,y2);
+}
 
+void find_distance(float x1, float y1, float x2, float y2, float *area)
+{
+  *area=sqrt((x2-x1)*(x2-x1)-(y2-y1)*(y2-y1));
+}
+void output(float area)
+{
+  printf("Area: %f\n",area);
+}
+
+int main()
+{
+  float x1,y1,x2,y2,area;
+  input(&x1,&y1,&x2,&y2);
+  find_distance(x1,y1,x2,y2,&area);
+  output(area);
+  return 0;
 }
---
+++ p2original.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3)
+{
+  printf("Enter: ");
+  scanf("%f%f%f%f%f%f",x1,y1,x2,y2,x3,y3);
+}
+
+int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
+
+void output(float x1, float y1, float x2, float y2,float x3, float y3, int istriangle)
